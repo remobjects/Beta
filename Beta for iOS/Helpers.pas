b285f011c4fb52832ee3aeceec3dc3f2ce4a825a
marc hoffman <mh@remobjects.com>
2017-12-01 15:55:24 -0400

    Some forgotten commits.

--- Beta for iOS/Helpers.pas
+++ Beta for iOS/Helpers.pas
@@ -1,15 +1,5 @@
 ﻿namespace Beta;
 
-interface
-
-uses
-  Foundation;
-
-extension method NSArray.distinctArrayWithKey(aKey: String): NSArray;
-extension method NSDate.relativeDateString: String;
-
-implementation
-
 extension method NSArray.distinctArrayWithKey(aKey: String): NSArray;
 begin
   var keyValues := new NSMutableSet;
@@ -21,51 +11,6 @@ begin
      keyValues.addObject(keyForItem);
     end;
   end;
-end;
-
-extension method NSDate.relativeDateString: String;
-const SECOND = 1;
-const MINUTE = 60 * SECOND;
-const HOUR = 60 * MINUTE;
-const DAY = 24 * HOUR;
-const MONTH = 30 * DAY;
-begin
-
-  var lNow := NSDate.date;
-  var lDelta := lNow.timeIntervalSinceDate(self);
-
-  var lCalendar := NSCalendar.currentCalendar;
-  var lUnits := (NSCalendarUnit.NSYearCalendarUnit or 
-                 NSCalendarUnit.NSMonthCalendarUnit or 
-                 NSCalendarUnit.NSDayCalendarUnit or 
-                 NSCalendarUnit.NSHourCalendarUnit or 
-                 NSCalendarUnit.NSMinuteCalendarUnit or 
-                 NSCalendarUnit.NSSecondCalendarUnit);
-  var lComponents := lCalendar.components(lUnits) fromDate(self) toDate(lNow) options(0);
-
-  {if (lDelta < 0) then
-    result := '!n the future!'
-  else if (lDelta < 1 * MINUTE) then
-    result := if (lComponents.second = 1) then 'One second ago' else NSString.stringWithFormat('%d seconds ago', lComponents.second) as NSString
-  else if (lDelta < 2 * MINUTE) then
-    result := 'a minute ago'
-  else if (lDelta < 45 * MINUTE) then
-    result := NSString.stringWithFormat('%d minutes ago', lComponents.minute)
-  else if (lDelta < 90 * MINUTE) then
-    result := 'an hour ago'
-  else if (lDelta < 24 * HOUR) then
-    result := NSString.stringWithFormat('%d hours ago', lComponents.hour)}
-  if (lDelta < 24 * HOUR) then
-    result := NSString.stringWithFormat('today')
-  else if (lDelta < 48 * HOUR) then
-    result := 'yesterday'
-  else if (lDelta < 30 * DAY) then
-    result := NSString.stringWithFormat('%ld days ago', lComponents.day)
-  else if (lDelta < 12 * MONTH) then
-    result := if (lComponents.month <= 1) then 'one month ago' else NSString.stringWithFormat('%ld months ago', lComponents.month) as NSString
-  else
-    result := if (lComponents.year <= 1) then 'one year ago' else NSString.stringWithFormat('%ld years ago', lComponents.year) as NSString;
-
 end;
 
 end.
---
+++ Beta for iOS/NSDate_Helpers.pas
@@ -0,0 +1,56 @@
+﻿namespace Beta;
+
+uses
+  Foundation;
+
+type
+  NSDate_Helpers = extension class (NSDate)
+  private
+    const SECOND = 1;
+    const MINUTE = 60 * SECOND;
+    const HOUR = 60 * MINUTE;
+    const DAY = 24 * HOUR;
+    const MONTH = 30 * DAY;
+  public
+    method relativeDateString: String;
+    begin
+      var lNow := NSDate.date;
+      var lDelta := lNow.timeIntervalSinceDate(self);
+    
+      var lCalendar := NSCalendar.currentCalendar;
+      var lUnits := (NSCalendarUnit.NSYearCalendarUnit or 
+                     NSCalendarUnit.NSMonthCalendarUnit or 
+                     NSCalendarUnit.NSDayCalendarUnit or 
+                     NSCalendarUnit.NSHourCalendarUnit or 
+                     NSCalendarUnit.NSMinuteCalendarUnit or 
+                     NSCalendarUnit.NSSecondCalendarUnit);
+      var lComponents := lCalendar.components(lUnits) fromDate(self) toDate(lNow) options(0);
+    
+      {if (lDelta < 0) then
+        result := '!n the future!'
+      else if (lDelta < 1 * MINUTE) then
+        result := if (lComponents.second = 1) then 'One second ago' else NSString.stringWithFormat('%d seconds ago', lComponents.second) as NSString
+      else if (lDelta < 2 * MINUTE) then
+        result := 'a minute ago'
+      else if (lDelta < 45 * MINUTE) then
+        result := NSString.stringWithFormat('%d minutes ago', lComponents.minute)
+      else if (lDelta < 90 * MINUTE) then
+        result := 'an hour ago'
+      else if (lDelta < 24 * HOUR) then
+        result := NSString.stringWithFormat('%d hours ago', lComponents.hour)}
+      if (lDelta < 24 * HOUR) then
+        result := NSString.stringWithFormat('today')
+      else if (lDelta < 48 * HOUR) then
+        result := 'yesterday'
+      else if (lDelta < 30 * DAY) then
+        result := NSString.stringWithFormat('%ld days ago', lComponents.day)
+      else if (lDelta < 12 * MONTH) then
+        result := if (lComponents.month <= 1) then 'one month ago' else NSString.stringWithFormat('%ld months ago', lComponents.month) as NSString
+      else
+        result := if (lComponents.year <= 1) then 'one year ago' else NSString.stringWithFormat('%ld years ago', lComponents.year) as NSString;
+    
+    end;
+    
+  end;
+
+end.
